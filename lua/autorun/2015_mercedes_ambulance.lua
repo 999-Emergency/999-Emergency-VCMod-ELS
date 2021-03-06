f491d25cf6c94f92b0ade6d181bcf767105bb4f3
mattdavidj <69766912+mattdavidj@users.noreply.github.com>
2021-03-06 17:23:49 +0000

    Add files via upload

---
+++ lua/autorun/2015_mercedes_ambulance.lua
@@ -0,0 +1,17 @@
+local V = {
+	-- Required information
+	Name = "2015 mercedes ambulance",
+	Model = "models/perrynsvehicles/2015_mercedes_nhs_ambulance/2015_vcmod_mercedes_ambulan.mdl",
+	Class = "prop_vehicle_jeep",
+	Category = "999Emergency - VCMod",
+
+	-- Optional information
+	Author = "MattDavidJ",
+	Information = "uwu",
+
+	KeyValues = {
+
+		vehiclescript = "scripts/vehicles/perryn/ambulance1.txt"
+	}
+}
+list.Set( "Vehicles", "ambulance1", V )
