e1924a5f88ea0cda485103f1f3a177de4f2fe4ef
mattdavidj <69766912+mattdavidj@users.noreply.github.com>
2021-05-03 00:13:59 +0100

    Add files via upload

---
+++ lua/autorun/emerg999sprintam.lua
@@ -0,0 +1,17 @@
+local V = {
+	-- Required information
+	Name = "2014 Mercedes-Benz Sprinter Ambulance",
+	Model = "models/smcars/2014_sprinter_ambulance.mdl",
+	Class = "prop_vehicle_jeep",
+	Category = "999Emergency - VCMod",
+
+	-- Optional information
+	Author = "Super Mighty",
+	Information = "Custom 2014 Mercedes-Benz Sprinter Ambulance for 999 Emergency funded by Relman.",
+
+	KeyValues = {
+
+		vehiclescript = "scripts/vehicles/superscars/2014_sprinter_ambulance.txt"
+	}
+}
+list.Set( "Vehicles", "999ambulance", V )
