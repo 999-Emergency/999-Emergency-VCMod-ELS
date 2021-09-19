e4ce75b97caf3bf1140680192d54fc292b230cd2
Super Mighty <supermighty1@gmail.com>
2021-09-19 16:01:29 +0100

    VcMod v2

---
+++ lua/autorun/metrohd_vw_multivan999.lua
@@ -0,0 +1,16 @@
+local V = {
+	-- Required information
+	Name = "LRPC VW Multivan",
+	Model = "models/metrohd/LRPC_T6VAN1.mdl",
+	Class = "prop_vehicle_jeep",
+	Category = "999Emergency - VCMod",
+
+	-- Optional information
+	Author = "MetroHD",
+	Information = "Vr00m vr00m",
+
+	KeyValues = {
+		vehiclescript = "scripts/vehicles/metrohd/vw_multivan.txt"
+	}
+}
+list.Set( "Vehicles", "LRPC_T6VAN1", V )
